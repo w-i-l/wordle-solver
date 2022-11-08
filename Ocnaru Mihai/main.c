55d437f0d5e1794a2d5ddc335d45ed1ded6e9ea1
GeeksforGeeks <masterpaper01@gmail.com>
2022-11-08 21:43:51 +0200

    Revert "generate_pro"
    This reverts commit 62d2fa6108eb0abdcb574269fb7d86cee4b4662c.

--- Ocnaru Mihai/main.c
+++ Ocnaru Mihai/main.c
@@ -17,7 +17,7 @@ char good_letters[6];//literele care apar in enter_word
 int index_good_letters = 0;
 char bad_letters[26];//literele care nu apar in enter_word
 int index_bad_letters = 0;
-char word_structure[6];//daca o litera apare pe locul potrivit
+char word_structure[6];//salveaza literele care sunt pe pozitiile bune
 
 
 //structura de stocare a literelor din fisier
@@ -155,22 +155,25 @@ void check_letters(FILE *file){
     for(int index_chosen_word=0;index_chosen_word<5;index_chosen_word++){
         for(int index_enter_word=0;index_enter_word<5;index_enter_word++){
             if(enter_word[index_enter_word] == chosen_word[index_chosen_word]){
-                int can_add = 1;
-
-                for(int i=0;i<index_good_letters;i++){
-                    if(enter_word[index_enter_word] == good_letters[i]){//daca exista deja litera
-                        can_add = 0;
-                        break;
-                    }
-                }
-
-                if(can_add){
-                    good_letters[index_good_letters] = enter_word[index_enter_word];
-                    index_good_letters++;
-                }
                 if(index_chosen_word == index_enter_word){//daca sunt pe aceeasi pozitie{
-                    word_structure[index_chosen_word] = enter_word[index_chosen_word];
 
+                }
+
+                else{//daca doar apare litera
+                    //daca litera nu apare in good-letters
+                    int can_add = 1;
+
+                    for(int i=0;i<index_good_letters;i++){
+                        if(enter_word[index_enter_word] == good_letters[i]){//daca exista deja litera
+                            can_add = 0;
+                            break;
+                        }
+                    }
+
+                    if(can_add){
+                        good_letters[index_good_letters] = enter_word[index_enter_word];
+                        index_good_letters++;
+                    }
                 }
             }
 
@@ -220,109 +223,6 @@ void display_bad_letters(){
     printf("\n");
 }
 
-void display_word_structure(){
-    printf("Word structure:\t");
-    for(int i=0;i<5;i++){
-        printf("%c ",word_structure[i]? word_structure[i]:'_');
-    }
-    printf("\n");
-}
-
-//initializeaza cu valori nule structure letters
-void empty_letters(){
-    for(int i=0;i<LETTERS_COUNT;i++){
-        letters[i].first_index = 0;
-        letters[i].letter = i+A_ASCII_INDEX;
-        letters[i].letter_count = 0;
-    }
-}
-
-//verifiaca daca cuvantul are litere fixate
-//returneaza indicele primei litere fixate
-//sau -1 daca nu exista
-int does_word_have_structure(){
-    for(int i=0;i<5;i++){
-        if(word_structure[i]!=0){
-            return i;
-        }
-    }
-    return -1;
-}
-
-////refa-o !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-void get_letters_count_pro(FILE *file){
-
-
-    fseek(file,0,SEEK_SET);
-    //creem o variabila pentru a stoca valoarea
-    int does_word_have_structure_variable = does_word_have_structure();
-
-    //mutam cursorul la pozitia initiala
-    //daca gasim o litera fixata
-    if(does_word_have_structure_variable != -1){
-        //schimbam offsetul la indexul primei litere fixate
-        //daca structura cuvantului este __R__ mutam offesetul la 2
-        //indicele lui R
-        fseek(file,does_word_have_structure_variable,SEEK_SET);
-
-    }
-    char c,l;
-    //iteram tot fisierul
-    while(EOF != (c=getc(file))){
-        if(c!=' ' && c!='\n'){
-            //daca litera din cuvantul curent este aceeasi
-            //cu cea din structura cuvantului cautat
-            if(c == word_structure[does_word_have_structure_variable]){
-                //ne mutam la inceputul cuvantului
-                fseek(file,-does_word_have_structure_variable-1,SEEK_CUR);
-
-                //daca cuvantul prezent are aceasi structura ca cel ales
-                int can_continue = 1;
-
-                //verificam structura cuvantului prezent
-                for(int i=0;i<5;i++){
-                    if(word_structure[i] != 0 && (l=getc(file))==word_structure[i] && l!='\n'){
-                        continue;
-                    }
-                    else{
-                        //nu am gasit aceeasi structura
-                        can_continue = 0;
-                        break;
-                    }
-                }
-
-                //daca cuvantul ales are aceeasi structura
-                if(can_continue){
-                    //mutam cursorul la inceputul cuvantului
-                    fseek(file,-6,SEEK_CUR);
-
-                    for(int i=0;i<5;i++){
-                        //luam litera curenta
-                        l = getc(file);
-                        letters[l%A_ASCII_INDEX].letter_count++;
-
-                    }
-
-                }
-            }
-
-
-
-//            letters[c%A_ASCII_INDEX].letter = c;
-//            letters[c%A_ASCII_INDEX].letter_count ++;
-//            //to find first index at the start of the word of the letters in txt
-//            //cuvintele sunt stocate pe 7 bytes incepand de la index 1
-//            if(ftell(file)%7==1 && letters[c%A_ASCII_INDEX].first_index == 0){
-//                   letters[c%A_ASCII_INDEX].first_index = ftell(file);
-//            }
-        }
-        //parcurge din cuvant in  cuvant
-        fseek(file,6,SEEK_CUR);
-
-    }
-
-}
-
 
 int main()
 {
@@ -332,13 +232,12 @@ int main()
 
 
     //alegem cuvantul
-    //2371263127673
-    random_world(file,1234344554,chosen_word);
+    random_world(file,23712631276732183,chosen_word);
     printf("Choosen_Word: %s\n\n",chosen_word);
 
     //calculam probabilitatea
     get_letters_count(file);
-    display_letters_count();
+//    display_letters_count();
 //
 //    printf("\n");
 
@@ -349,23 +248,17 @@ int main()
     //gasim toate cuv cu aceleasi litere
     generate_all_anagrams(file,0);
 
-    printf("----------------\nEnter_word: %s\n\n",enter_word);
+    printf("Enter_word: %s\n\n",enter_word);
 
     check_letters(file);
 
     display_good_letters();
     display_bad_letters();
-    display_word_structure();
 
-    get_letters_count_pro(file);
 
-    printf("----------------\n%d\n",does_word_have_structure());
 
-    empty_letters();
 
-    get_letters_count_pro(file);
 
-    display_letters_count();
 
 
     fclose(file);
