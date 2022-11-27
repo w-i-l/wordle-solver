761694fe2e7587eeff07f1d83a1ca51e9a12bb2e
unknown <masterpaper01@gmail.com>
2022-11-27 16:51:44 +0200

    Final update

--- Pc/second.py
+++ Pc/second.py
@@ -2,21 +2,32 @@ from math import log2
 import time
 import random
 
+#File with all words
 f = open("cuvinte_wordle.txt",'r')
+#Intercomunicating file 
 out_txt = open("out.txt",'a')
-# g = open("output.txt",'w',encoding='utf-8')
+#File with generated structure
+g = open("output.txt",'w',encoding='utf-8')
+#Lists with all words
 words = [x.strip() for x in f]
 f = open("cuvinte_wordle.txt",'r')
 words2 = [x.strip() for x in f]
-# words = ["TAREI",'ZULIE', 'ZULUF', 'ZVELT']
 
+#Best word used to guess
 best_word = ''
 max_entropy = 0
 
+#Calculate the entropy for a word
 def Entropy(word):
+
+    #Dictionarry with all patterns generated
+    #for the word variable
     patterns={}
+
     for x in words:
+        #Pattern for a word related to given one
         pattern=''
+
         for i in range(5):
             if x[i]== word[i]:
                 pattern+='🟩'
@@ -24,18 +35,24 @@ def Entropy(word):
                 pattern+='🟨'    
             else:
                 pattern+='⬜'
+        
+        #Add to dict
         if pattern in patterns:
             patterns[pattern]+=1
         else:
             patterns[pattern]=1
-        # print(pattern)
+
+    #The return entropy value        
     entropy=0
     for x in patterns:
         
+        #Compute the probability
         probability=patterns[x]/len(words)
         entropy+=probability*log2(probability)
+
     return -entropy
 
+#Find the best word that fits
 def find_best_word():
     global best_word,max_entropy
 
@@ -44,20 +61,24 @@ def find_best_word():
 
     for word in words:
         entropy = Entropy(word)
-
+        
+        #If the current word has the
+        #greatest entropy
         if entropy > max_entropy and len(words)>1:
             best_word  = word
             max_entropy = entropy
+
+        #If there is only one word left
         elif len(words) == 1:
             best_word = words[0]
 
-
+#Check the word if it fits the pattern
 def check_word(word,pattern):
     
 
     if len(pattern) <5:
         return 0
-    # print(pattern)
+
     for i in range(5):
         if pattern[i] == '⬜':
             if best_word[i] in word:
@@ -73,7 +94,7 @@ def check_word(word,pattern):
                 return 0
     return 1
 
-            
+#Deletes the word that doesn't fit              
 def check_letters():
     global words
 
@@ -87,7 +108,8 @@ def check_letters():
         if check_word(word,pattern) == 0:
             words.remove(word)
     
-
+#For a given word and structure 
+#finds thw word that match
 def generate_structure(given_word,given_structure):
     for word in words:
         pattern = ''
@@ -101,6 +123,8 @@ def generate_structure(given_word,given_structure):
         if pattern == given_structure:
             return word
 
+#Writes the structure for a chosen word
+#related to the current best word
 def output_joc(chosen_word):
     g = open("output.txt",'w',encoding='utf-8')
     pattern = ''
@@ -116,45 +140,41 @@ def output_joc(chosen_word):
     return pattern
 
 chosen_word = ''
-# chosen_word = "ORALA"
-
 response = ''
 
+#Wait until the the contet of 
+#intercomunicating file changes
 def wait_for_response():
     global response
+    #File used to intercomunicate
     output_txt = open('output.txt','r+',encoding='utf-8')
     pattern = output_txt.read()
     output_txt.seek(0)
+
+    #If the readed file content is not good
     while pattern == output_txt.read() or not(len(response)>0 and response[0]  in ['⬜','🟨','🟩']):
-        # time.sleep(0.5)
         output_txt.seek(0)
-        # print(output_txt.read())
         response = output_txt.read()
-        
         output_txt = open('output.txt','r+',encoding='utf-8')
     
     output_txt.close()
 
+#Writes the best word to output.txt
 def write_best_word():
      output_txt = open('output.txt','w',encoding='utf-8')
      output_txt.write(best_word)
 
+#Main function call
 def solve():
     global chosen_word,best_word,max_entropy,response
-
     wait_for_response()
     check_letters()
     find_best_word()
     write_best_word()
-    # print(best_word,output_joc(chosen_word))
-    
-    # print(words)
-
-
-    
 
 best_word = "TAREI"
 
+#Used to test no_of_cases words from file
 def tester(no_of_cases):
     global chosen_word,best_word,max_entropy,words
 
@@ -166,20 +186,22 @@ def tester(no_of_cases):
     for i in range(no_of_cases):
         f.seek(0)
         words = [x.strip() for x in f]
+
         chosen_word = words2[i]
         best_word = 'TAREI'
         max_entropy = 0
+
         print('*'+chosen_word)
         out_txt.write('*'+chosen_word)
-        # print("*TAREI")
-        out_txt.write(" || TAREI")
-        
+        print("*TAREI")
+        out_txt.write(" TAREI")
         output_joc(chosen_word)
+        
         i=1
         while not chosen_word == best_word:
             solve()
             # print("*"+best_word)
-            out_txt.write(" --> "+best_word)
+            out_txt.write(" "+best_word)
             i += 1
         if chosen_word == best_word:
             teste_ok += 1
@@ -196,22 +218,7 @@ def write_tarei():
     f = open('output.txt','w',encoding='utf-8')
     f.write("TAREI")
 
-
-# output_txt = open('output.txt','r',encoding='utf-8')
-# print(output_txt.read())
-# print(response)
-# tester(1)
-
-# chosen_word = "IROSI"
-# print('=='+chosen_word)
-
-# output_txt = open('output.txt','w',encoding='utf-8')
-# output_txt.write("TAREI")
-# for x in range(3):
-#     wait_for_response()
-#     solve()
-#     print(best_word)
-# print(output_joc(chosen_word))
+#Generate bot
 def auto():
     print("*TAREI")
     write_tarei()
@@ -224,5 +231,3 @@ def auto():
         i += 1
     print("Incercari: ",i)
     print(time.time()-before)
-
-# auto()
